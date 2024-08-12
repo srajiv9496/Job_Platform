18feaba42f2adc3d868e4a5bde2e8831d9f6c7e5
srajiv9496 <srajiv9496@gmail.com>
2024-08-12 18:52:55 +0530

    Designing the Homepage

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pixel Positions</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    @vite(['resources/css/app.css','resources/js/app.js'])
+</head>
+<body class="bg-black text-white font-hanken-grotesk">
+    <div class="px-10">
+        <nav class="flex justify-between items-center py-4 border-b border-white/10">
+            <div>
+                <a href = "/">
+                    <img src="{{\Illuminate\Support\Facades\Vite::asset('resources/images/logo.png')}}" alt="logo">
+                </a>
+            </div>
+            <div class="space-x-6 font-bold">
+                <a href="#">Jobs</a>
+                <a href="#">Careers</a>
+                <a href="#">Salaries</a>
+                <a href="#">Companies</a>
+            </div>
+
+            <div>Post a job</div>
+
+        </nav>
+
+        <main class="mt-10 max-w-[986px] mx-auto">
+            {{$slot}}
+        </main>
+    </div>
+</body>
+</html>
---
+++ resources/views/components/section-heading.blade.php
@@ -0,0 +1,6 @@
+<div class="inline-flex items-center gap-x-2">
+    <h3 class="font-bold text-lg">
+        <span class="w-2 h-2 bg-white inline-block"></span>
+        {{$slot}}
+    </h3>
+</div>
